b53fa4e1bd1feab6dd948293b2c4909e4cd9d85a
shlomif <shlomif@52c325ad-5fd0-0310-8a0f-c43feede02cc>
2006-11-18 16:47:11 +0000

    Fixed CondTestMore so it'll work with the new Test::Builder::Module-based Test::More.

--- CondTestMore.pm
+++ CondTestMore.pm
@@ -1,24 +1,12 @@
-# This is an assisting module. It makes sure Test::More is present, and if not
-# gracefully exists. Otherwise it behaves the same as Test::More.
-# 
-
 package CondTestMore;
 
-use vars qw(@ISA);
+use base 'Test::More';
 
-@ISA=qw(Test::More);
+use Test::More;
 
 BEGIN
 {
-    eval {
-       require Test::More;
-    };
-    if ($@)
-    {
-        warn "You don't have Test::More. Terminating";
-        print "1..0\n";
-        exit 0;
-    }
+*EXPORT = \@Test::More::EXPORT;
 }
 
 1;
